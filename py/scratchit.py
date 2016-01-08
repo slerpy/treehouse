c91ed466492b8cf84275685140bfc80f9d894228
slerpy <slerpy@riseup.net>
2016-01-08 04:50:04 -0800

    placeholder, nothing to see here.

--- py/scratchit.py
+++ py/scratchit.py
@@ -10,8 +10,27 @@
 # @date 29 November 2015
 # @version 0.0.1
 ##############################################################################
-import random
-def random_item(iter):
-    print(iter[random.randint(0,len(iter) -1)])
 
-random_item("Random String")
+
+
+import random
+
+
+def even_odd(num):
+    # If % 2 is 0, the number is even.
+    # Since 0 is falsey, we have to invert it with not.
+    return not num % 2
+
+
+def ok():
+    start = 5
+    while start:
+        randomy = random.randint(1,99)
+        if even_odd(randomy) is True:
+            print("{} is even".format(randomy))
+            start -= 1
+        else:
+            print("{} is odd".format(randomy))
+            start -= 1
+
+ok()
