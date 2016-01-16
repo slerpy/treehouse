0f93e2fb2015ed81e94ea7efe52b6c5f63e4bd60
slerpy <slerpy@riseup.net>
2016-01-16 03:27:12 -0800

    trying out a few dictionary ideas.

--- py/scratchit.py
+++ py/scratchit.py
@@ -45,5 +45,65 @@ def word_count(strang):
     return easify_dict
 
 
-word_count("I am what I am.")
+dicts = [
+    {'name': 'Michelangelo',
+     'food': 'PIZZA'},
+    {'name': 'Garfield',
+     'food': 'lasanga'},
+    {'name': 'Walter',
+     'food': 'pancakes'},
+    {'name': 'Galactus',
+     'food': 'worlds'}
+]
+
+string = "Hi, I'm {name} and I'm fucking craving {food}!"
+
+
+def string_factory():
+    new_list = []
+    count = 0
+    for each in dicts:
+        new_list.append(string.format(**dicts[count]))
+        count += 1
+    return new_list
+
+
+traveler_dictionary = {'Random Person 01': ['Place Traveled 1', 'Place Traveled 2', 'Place Traveled 3'], 'Random Person 02': ['Place Traveled 01', 'Place Traveled 02'], 'Random Person 03': ['Place Traveled One', 'Place Traveled Two', 'Place Traveled Three', 'Place Traveled Four']}
+
+
+def most_traveled_person(travelers):
+    max_count = 0
+    current_winner = ""
+    for traveler in travelers:
+
+        if len(travelers[traveler]) > max_count:
+            max_count = len(travelers[traveler])
+            current_winner = traveler
+    print(current_winner)
+
+
+def num_travelers(travelers):
+    return len(travelers.values())
+
+
+def stats_travelers(travelers):
+    temp_list = []
+    for traveler in travelers:
+        temp_list.append([traveler, len(travelers[traveler])])
+    print(temp_list[0])
+
+def places_traveled(travelers):
+    traveled_list = []
+    for traveler in travelers:
+        for places in travelers[traveler]:
+            traveled_list.append(places)
+    print(traveled_list)
+
+places_traveled(traveler_dictionary)
+
+
+
+
+
+
 
